079bed7b9f189af8f56511e38ca80ac18ce3fdbc
Adam Hochberger <adamhochberger@gmail.com>
2020-05-22 15:07:43 -0400

    implemented abstract model  for post types

--- blog/models.py
+++ blog/models.py
@@ -2,16 +2,16 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
-class Post(models.Model):
+class Entry(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
-    summary = models.CharField(max_length=300)
-    content = models.TextField()
-    published = models.BooleanField(default=True)
+    summary = models.TextField(max_length=300)
+
     created = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(upload_to="images")
+    image = models.ImageField(upload_to="images", default="placeholder.png")
 
     class Meta:
+        abstract = True
         ordering = ['-created']
 
     def __unicode__(self):
@@ -19,4 +19,20 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse("blog.views.post", args=[self.slug])
-        
+
+class Post(Entry):
+    content = models.TextField(max_length=255, default="")
+
+    class Meta(Entry.Meta):
+        db_table = 'post_info'
+
+    published = models.BooleanField(default=True)
+
+class Project(Entry):
+    github_link = models.CharField(max_length=255, default="")
+    implementation_link = models.CharField(max_length=255, default="")
+
+    class Meta(Entry.Meta):
+        db_table = 'project_info'
+
+    published = models.BooleanField(default=True)
