0dffae6b1b754b42e77bab4c4ced65aba8b94462
Adam Hochberger <adamhochberger@gmail.com>
2020-05-25 17:15:17 -0400

    changes to display image

--- selfsite/urls.py
+++ selfsite/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from blog import views as blog_views
 
@@ -23,3 +25,6 @@ urlpatterns = [
     path('about/', blog_views.about),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
